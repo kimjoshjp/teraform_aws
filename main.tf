51c8495e5a465c9db2d31cc95b4da06ff44c8888
kimjoshjp <kimjoshjp@gmail.com>
2020-07-23 18:40:31 +1000

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 variable "aws_access_key" {}
 variable "aws_secret_key" {}
 
-provide "aws" {
+provider "aws" {
   access_key = var.aws_access_key
   secret_key = var.aws_secret_key
   region = "ap-southeast-2"
