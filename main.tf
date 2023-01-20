60555188329ebb1de63648b917523801b5e8ca52
Kazu Fukushige <kimjoshjp@gmail.com>
2023-01-20 18:11:52 +1000

    Update main.tf
    updated

--- main.tf
+++ main.tf
@@ -1,15 +1,18 @@
-variable "aws_access_key" {}
-variable "aws_secret_key" {}
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
+}
 
+# Configure the AWS Provider
 provider "aws" {
-  access_key = var.aws_access_key
-  secret_key = var.aws_secret_key
-  region = "ap-southeast-2"
-  }
+  region = "ap-southeast-1"
+}
 
-resource "aws_vpc" "test-vpc" {
-   cidr_block = "10.0.0.0/16"
-   tags = {
-     Name = "test-vpc"
-     }
-  }
+# Create a VPC
+resource "aws_vpc" "example" {
+  cidr_block = "10.0.0.0/16"
+}
