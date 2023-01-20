4ad69d043d573e3004e6e26571bc46ca21496fe9
Kazu Fukushige <kimjoshjp@gmail.com>
2023-01-20 22:12:04 +1000

    Update main.tf
    Added ec2 instance

--- main.tf
+++ main.tf
@@ -4,7 +4,16 @@ variable "aws_secret_key" {}
 provider "aws" {
   access_key = var.aws_access_key
   secret_key = var.aws_secret_key
-  region = "ap-northeast-1"
+  region = "us-west-2"
+}
+
+resource "aws_instance" "web" {
+  ami           = "ami-005e54dee72cc1d00" #us-west-2
+  instance_type = "t3.micro"
+
+  tags = {
+    Name = "HelloWorld"
+  }
 }
 
 resource "aws_vpc" "test-vpc" {
